c2093d6121db0d8875664eb3e066b1712692b0b7
SergeiGrub <sergeygrubber@gmail.com>
2024-07-26 13:47:31 +0300

    Final Test v 1.0

---
+++ FinalTest/src/main/java/ru/gb/FinalTest/FinalTestApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.FinalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTestApplication.class, args);
+	}
+
+}
